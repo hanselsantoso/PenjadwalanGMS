848fda1f77ff69c2a4d80741ac70ad7e41f406cb
Ventruo <lawrencepatrick0@gmail.com>
2025-01-28 21:10:01 +0700

    [Jadwal_Ibadah] Update CRUD

--- app/Http/Controllers/JadwalIbadahController.php
+++ app/Http/Controllers/JadwalIbadahController.php
@@ -10,14 +10,17 @@ class JadwalIbadahController extends Controller
 {
     public function jadwalIbadah()
     {
-        // TODO: IDK WHY KOK CUMA STATUS 1 YG DIMUNCULIN
-        $jadwal = JadwalIbadah::where('status_jadwal_ibadah', 1)->get();
-        return view('jadwal_ibadah', compact('jadwal'));
+        $jadwal = JadwalIbadah::get();
+        $jadwalActive = $jadwal->where('status_jadwal_ibadah', 1);
+        $jadwalInactive = $jadwal->where('status_jadwal_ibadah', 0);
+
+        return view('jadwal_ibadah', compact('jadwalActive', 'jadwalInactive'));
     }
 
     public function store(Request $request)
     {
-        //TODO: PENGECEKAN SEMUA JAM
+        $this->doValidate($request);
+
         $jadwalIbadah = new JadwalIbadah();
         $jadwalIbadah->nama_ibadah = $request->nama_ibadah;
         $jadwalIbadah->alias_ibadah = $request->alias_ibadah;
@@ -31,7 +34,13 @@ class JadwalIbadahController extends Controller
 
     public function update(Request $request)
     {
-        //TODO: PENGECEKAN SEMUA JAM
+        $this->doValidate($request);
+        $request->validate([
+            'id_jadwal_ibadah' => 'required'
+        ], [
+            'id_jadwal_ibadah.required' => 'ID Jadwal Ibadah harus diisi.'
+        ]);
+
         $jadwalIbadah = JadwalIbadah::find($request->id_jadwal_ibadah);
         $jadwalIbadah->nama_ibadah = $request->nama_ibadah;
         $jadwalIbadah->alias_ibadah = $request->alias_ibadah;
@@ -40,6 +49,27 @@ class JadwalIbadahController extends Controller
         $jadwalIbadah->jam_akhir = $request->jam_akhir;
         $jadwalIbadah->save();
         return redirect()->route('jadwal_ibadah_index')->with('success', 'Jadwal Ibadah berhasil diperbarui.');
+    }
+
+    private function doValidate(Request $request) {
+        $request->validate([
+            'nama_ibadah' => 'required|string|max:255',
+            'alias_ibadah' => 'required|string|max:255',
+            'jam_stand_by' => 'required|string',
+            'jam_mulai' => 'required|string',
+            'jam_akhir' => ['required', 'string', function($attribute, $value, $fail) use($request) {
+                $jamMulai = strtotime($request->jam_mulai);
+                $jamAkhir = strtotime($value);
+                if ($jamAkhir < $jamMulai) {
+                    $fail('Jam akhir harus lebih besar atau sama dengan jam mulai.');
+                }
+            }],
+        ], [
+            'nama_ibadah.required' => 'Nama Ibadah harus diisi.',
+            'nama_ibadah.max' => 'Nama Ibadah tidak boleh lebih dari 255 karakter.',
+            'alias_ibadah.required' => 'Alias Ibadah harus diisi.',
+            'alias_ibadah.max' => 'Alias Ibadah tidak boleh lebih dari 255 karakter.',
+        ]);
     }
 
     public function deactivate($id)
--- resources/views/jadwal_ibadah.blade.php
+++ resources/views/jadwal_ibadah.blade.php
@@ -13,20 +13,21 @@
                 </div>
             </div>
 
-            <table id="tabelUser" class="table table-striped table-bordered">
+            <h4>Jadwal Ibadah Aktif</h4>
+            <table id="tabelIbadahActive" class="table table-striped table-bordered">
                 <thead>
                     <tr>
                         <th width="10%">No</th>
-                        <th width="30%">Nama Ibadah</th>
-                        <th width="30%">Alias</th>
-                        <th width="15%">Jam Stand By</th>
-                        <th width="15%">Jam Mulai</th>
-                        <th width="15%">Jam Akhir</th>
+                        <th width="25%">Nama Ibadah</th>
+                        <th width="20%">Alias</th>
+                        <th width="10%">Jam Stand By</th>
+                        <th width="10%">Jam Mulai</th>
+                        <th width="10%">Jam Akhir</th>
                         <th width="15%">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($jadwal as $item)
+                    @foreach ($jadwalActive as $item)
                     <tr>
                         <input type="hidden" name="id_jadwal_ibadah" value="{{$item["id_jadwal_ibadah"] }}">
                         <input type="hidden" name="nama_ibadah" value="{{$item["nama_ibadah"] }}">
@@ -45,17 +46,56 @@
                             <a href="#" class="btn btn-warning buttonEdit w-100 mb-2" data-toggle="modal" data-target="#updateJadwalIbadah">
                                 Update
                             </a>
-                            @if ($item->status_jadwal_ibadah == 1)
-                                <form action="/admin/jadwal_ibadah/deactivate/{{ $item["id_jadwal_ibadah"] }}" method="post">
-                                    @csrf
-                                    <button type="submit" class="btn btn-danger w-100">Suspend</button>
-                                </form>
-                            @else
-                                <form action="/admin/jadwal_ibadah/activate/{{ $item["id_jadwal_ibadah"] }}" method="post">
-                                    @csrf
-                                    <button type="submit" class="btn btn-success w-100">Aktifkan</button>
-                                </form>
-                            @endif
+                            <form action="/admin/jadwal_ibadah/deactivate/{{ $item["id_jadwal_ibadah"] }}" method="post">
+                                @csrf
+                                <button type="submit" class="btn btn-danger w-100">Suspend</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+
+                </tbody>
+            </table>
+
+            <br>
+            <hr/>
+            <h4>Jadwal Ibadah Tidak Aktif</h4>
+            <table id="tabelIbadahInactive" class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th width="10%">No</th>
+                        <th width="25%">Nama Ibadah</th>
+                        <th width="20%">Alias</th>
+                        <th width="10%">Jam Stand By</th>
+                        <th width="10%">Jam Mulai</th>
+                        <th width="10%">Jam Akhir</th>
+                        <th width="15%">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($jadwalInactive as $item)
+                    <tr>
+                        <input type="hidden" name="id_jadwal_ibadah" value="{{$item["id_jadwal_ibadah"] }}">
+                        <input type="hidden" name="nama_ibadah" value="{{$item["nama_ibadah"] }}">
+                        <input type="hidden" name="alias_ibadah" value="{{$item["alias_ibadah"] }}">
+                        <input type="hidden" name="jadwal_stand_by" value="{{$item["jam_stand_by"] }}">
+                        <input type="hidden" name="jadwal_mulai" value="{{$item["jam_mulai"] }}">
+                        <input type="hidden" name="jadwal_akhir" value="{{$item["jam_akhir"] }}">
+                        <td>{{ $loop->index + 1 }}</td>
+                        <td> {{$item["nama_ibadah"] }}</td>
+                        <td> {{$item["alias_ibadah"] }}</td>
+                        <td> {{$item["jam_stand_by"] }}</td>
+                        <td> {{$item["jam_mulai"] }}</td>
+                        <td> {{$item["jam_akhir"] }}</td>
+
+                        <td>
+                            <a href="#" class="btn btn-warning buttonEdit w-100 mb-2" data-toggle="modal" data-target="#updateJadwalIbadah">
+                                Update
+                            </a>
+                            <form action="/admin/jadwal_ibadah/activate/{{ $item["id_jadwal_ibadah"] }}" method="post">
+                                @csrf
+                                <button type="submit" class="btn btn-success w-100">Activate</button>
+                            </form>
                         </td>
                     </tr>
                 @endforeach
@@ -66,19 +106,6 @@
     </div>
 
     <!-- Create User Modal -->
-    @php
-    function generateTimeOptions() {
-        $options = [];
-        for ($hour = 0; $hour < 24; $hour++) {
-            $time1 = sprintf("%02d:00", $hour);
-            $time2 = sprintf("%02d:30", $hour);
-            $options[] = "<option value=\"{$time1}\">{$time1}</option>";
-            $options[] = "<option value=\"{$time2}\">{$time2}</option>";
-        }
-        $options[] = "<option value=\"24:00\">24:00</option>";
-        return implode("\n", $options);
-    }
-    @endphp
     <div class="modal fade" id="createJadwalIbadah">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -99,23 +126,17 @@
                             <input type="text" class="form-control" name="alias_ibadah" required>
                         </div>
                         <div class="form-group mb-1">
-                            <label for="nama_tag">Jam Stand By</label>
-                            <select class="form-control" name="jam_stand_by" id="jam_stand_by">
-                                {!! generateTimeOptions() !!}
-                            </select>
+                            <label for="jam_stand_by">Jam Stand By</label>
+                            <input class="form-control" type="time" id="jam_stand_by" name="jam_stand_by" value="00:00" required>
                         </div>
                         <div class="form-group mb-1">
-                            <label for="nama_tag">Jam Ibadah Mulai</label>
-                            <select class="form-control" name="jam_mulai" id="jam_mulai">
-                                {!! generateTimeOptions() !!}
-                            </select>
+                            <label for="jam_mulai">Jam Ibadah Mulai</label>
+                            <input class="form-control" type="time" id="jam_mulai" name="jam_mulai" value="00:00" required>
                         </div>
 
                         <div class="form-group mb-1">
-                            <label for="nama_tag">Jam Ibadah Selesai</label>
-                            <select class="form-control" name="jam_akhir" id="jam_akhir">
-                                {!! generateTimeOptions() !!}
-                            </select>
+                            <label for="jam_akhir">Jam Ibadah Selesai</label>
+                            <input class="form-control" type="time" id="jam_akhir" name="jam_akhir" value="00:00" required>
                         </div>
 
                     </div>
@@ -153,23 +174,17 @@
                                 <input type="text" class="form-control" name="alias_ibadah" required>
                             </div>
                             <div class="form-group mb-1">
-                                <label for="nama_tag">Jam Stand By</label>
-                                <select class="form-control" name="jam_stand_by" id="jam_stand_by">
-                                    {!! generateTimeOptions() !!}
-                                </select>
+                                <label for="jam_stand_by">Jam Stand By</label>
+                                <input class="form-control" type="time" id="jam_stand_by" name="jam_stand_by" value="00:00" required>
                             </div>
                             <div class="form-group mb-1">
-                                <label for="nama_tag">Jam Ibadah Mulai</label>
-                                <select class="form-control" name="jam_mulai" id="jam_mulai">
-                                    {!! generateTimeOptions() !!}
-                                </select>
+                                <label for="jam_mulai">Jam Ibadah Mulai</label>
+                                <input class="form-control" type="time" id="jam_mulai" name="jam_mulai" value="00:00" required>
                             </div>
 
                             <div class="form-group mb-1">
-                                <label for="nama_tag">Jam Ibadah Selesai</label>
-                                <select class="form-control" name="jam_akhir" id="jam_akhir">
-                                    {!! generateTimeOptions() !!}
-                                </select>
+                                <label for="jam_akhir">Jam Ibadah Selesai</label>
+                                <input class="form-control" type="time" id="jam_akhir" name="jam_akhir" value="00:00" required>
                             </div>
 
                         </div>
@@ -190,7 +205,12 @@
 
 @section('script')
 <script>
-    $('#tabelUser').DataTable({
+    $('#tabelIbadahActive').DataTable({
+        "paging": true,
+        "pageLength": 10,
+    });
+
+    $('#tabelIbadahInactive').DataTable({
         "paging": true,
         "pageLength": 10,
     });
@@ -204,7 +224,7 @@
                 var value = $(this).val();
                 data.push(value);
             });
-            console.log(data);
+
             let id = data[0];
             let nama_ibadah = data[1];
             let alias_ibadah = data[2];
@@ -215,9 +235,9 @@
             $('#updateJadwalIbadah').find('input[name="id_jadwal_ibadah"]').val(id);
             $('#updateJadwalIbadah').find('input[name="nama_ibadah"]').val(nama_ibadah);
             $('#updateJadwalIbadah').find('input[name="alias_ibadah"]').val(alias_ibadah);
-            $('#updateJadwalIbadah').find('select[name="jam_stand_by"]').val(stand);
-            $('#updateJadwalIbadah').find('select[name="jam_mulai"]').val(awal);
-            $('#updateJadwalIbadah').find('select[name="jam_akhir"]').val(akhir);
+            $('#updateJadwalIbadah').find('input[name="jam_stand_by"]').val(stand);
+            $('#updateJadwalIbadah').find('input[name="jam_mulai"]').val(awal);
+            $('#updateJadwalIbadah').find('input[name="jam_akhir"]').val(akhir);
         });
     });
 
